2ace5ee0f902ddaa104aeaaba26315b675e19072
timurkazan99 <timurkazan99@gmail.com>
2023-01-21 14:42:04 +0300

    add img

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -5,18 +5,20 @@ const statusMap = {
   'armor': '/assets/img/armor.svg#armor',
   'attention': '/assets/img/attention.svg#attention',
   'speed': '/assets/img/speed.svg#speed',
-  'initiative': '/assets/img/initiative.svg#initiative',
+  'initiativeBonus': '/assets/img/initiative.svg#initiative',
+  'add': '/assets/img/add.svg#add',
+  'hp': '/assets/img/hp.svg#hp',
+  'delete': '/assets/img/delete.svg#delete',
+  'previous': '/assets/img/previous.svg#previous',
+  'next': '/assets/img/next.svg#next'
 }
 
-const Icon = ({icon, value, className}) => {
+const Icon = ({icon, className}) => {
   const logo = statusMap[icon];
   return (
-    <div className={className}>
-      <svg className="bi-icon" style={{height: '16px', width: '16px'}}>
-        <use href={logo}></use>
-      </svg>
-      <label>{value}</label>
-    </div>
+    <svg className={`bi-icon ${className}`} style={{height: '16px', width: '16px'}}>
+      <use href={logo}></use>
+    </svg>
   );
 };
 
