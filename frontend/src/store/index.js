d5a730a41a820c1870e72c5c18ebf81d35a61452
timurkazan99 <timurkazan99@gmail.com>
2023-01-21 19:29:20 +0300

    squash! refactoring

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,16 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { reducer as abilitiesSlice } from './reducers/abilitiesSlice';
 import { reducer as skillsSlice } from './reducers/skillsSlice';
+import { reducer as uiSlice } from './reducers/UiSlice';
+import { reducer as cardSlice } from './reducers/CardSlice';
+import { reducer as creatureSlice } from './reducers/CreatureSlice';
 
 const rootReducer = combineReducers({
   abilities: abilitiesSlice,
   skills: skillsSlice,
+  ui: uiSlice,
+  cards: cardSlice,
+  creatures: creatureSlice,
 });
 
 export default function setupStore() {
