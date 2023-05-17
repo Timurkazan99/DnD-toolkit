67b34c477f96b91bd36adf72b8cb26414b720010
timurkazan99 <timurkazan99@gmail.com>
2023-05-17 23:03:38 +0300

    fix uri

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,4 +1,4 @@
 export const URI = process.env.REACT_APP_URI || '';
 
-export const BATTLE_ROUTE = `${URI}/`;
-export const NOTFOUND_ROUTE = `${URI}/404`;
+export const BATTLE_ROUTE = `/${URI}/`;
+export const NOTFOUND_ROUTE = `/${URI}/404`;
